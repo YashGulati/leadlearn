f16ddc84d4c89cdab52f5a2a83af5a02fae80ebb
yash <yashgulati.g1@gmail.com>
2017-04-13 12:19:31 +0530

     

--- dbs/getQuestion.js
+++ dbs/getQuestion.js
@@ -2,7 +2,7 @@ var question = require('./questionSchema')
 // var global = require('./global')
 
 var renderQuestion = function(req, res){
-  let weapon = req.query['weapon'];
+  var weapon = req.query['weapon'];
   if (weapon === 'all') weapon = /.*/i;
   question.findRandom({tags: weapon}).limit(5).exec(function (err, question) {
   console.log(question);
